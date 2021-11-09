4d87a642e88716414faad9bba82b037458613f09
Danielp <64538910+Danielpovolni@users.noreply.github.com>
2021-11-09 19:12:22 +0100

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int euclid(int number1, int number2){
+    int division = number1 / number2;
+    int times_division = number1 - (number2 * division);
+    cout << times_division << endl;
+  if(division == 0){
+    return 1;
+  }else{
+    return euclid(number2, times_division);
+  }
+}
+int main(){
+  cout << euclid(918, 252) << endl;
+}
